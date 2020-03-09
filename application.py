22d24c40e499efb66f4daf1614ba938bc3f210fb
Wynand Tredoux <wynand.tredoux@ucalgary.ca>
2020-03-08 21:31:43 -0600

    removed unnecessary imports from application.py

--- application.py
+++ application.py
@@ -1,24 +1,10 @@
-import os
-import requests
-
-from flask import Flask, session, request, render_template, redirect, url_for, abort, jsonify
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-
-# function to deal with quotes in SQL queries
-def SQLquotes(str):
-    if "'" in str:
-        return str.replace("'", "''")
-    else:
-        return str
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 # Load config
 app.config.from_pyfile('config.cfg', silent=True)  # load settings from config.cfg
-Session(app)
+#Session(app)
 
 @app.route("/")
 def home():
